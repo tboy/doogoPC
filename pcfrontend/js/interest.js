279db51ce82968bf921afc42409512c03af3d164
fudehong <fudehong@qq.com>
2019-11-18 08:10:14 +0800

    #666

---
+++ pcfrontend/js/interest.js
@@ -0,0 +1,46 @@
+
+     var interestData=[
+            {
+                "id": 0,
+				"parent_id": 0,
+                "name": "服饰箱包",
+                "parent_name": ""
+            },
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "时尚女装",
+			    "parent_name": "服饰箱包"
+			},
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "时尚男装",
+			    "parent_name": "服饰箱包"
+			},
+
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "时尚男装1",
+			    "parent_name": "服饰箱包"
+			},
+			 {
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "体育运动",
+			    "parent_name": ""
+			},
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "体育运动1",
+			    "parent_name": "体育运动"
+			},
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "体育运动2",
+			    "parent_name": "体育运动"
+			}
+        ]
---
+++ pcfrontend/js/mediaData.js
@@ -0,0 +1,46 @@
+
+     var mediaData=[
+            {
+                "id": 0,
+				"parent_id": 0,
+                "name": "通用",
+                "parent_name": ""
+            },
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "58同城",
+			    "parent_name": "通用"
+			},
+			{
+			    "id": 0,
+				"parent_id": 0,
+			    "name": "58同城A",
+			    "parent_name": "58同城"
+			},
+
+			{
+			     "id": 0,
+			    "parent_id": 0,
+			    "name": "58同城B",
+			    "parent_name": "58同城"
+			},
+			 {
+			     "id": 0,
+			    "parent_id": 0,
+			    "name": "58同城c",
+			    "parent_name": "58同城"
+			},
+			{
+			    "id": 0,
+			   "parent_id": 0,
+			   "name": "58同城d",
+			   "parent_name": "58同城"
+			},
+			{
+			     "id": 0,
+			    "parent_id": 0,
+			    "name": "58同城e",
+			    "parent_name": "58同城"
+			}
+        ]
